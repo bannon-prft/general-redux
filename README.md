36a81472240f866f40c3ab9b36ac67bcfea09974
Bannon Tanner <bannon.tanner@perficient.ccom>
2022-03-22 15:46:09 -0500

    redux basics, useSelector, useDispatch, connect, createSlice, configureStore, README update

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ This is the redux portion from [React - The Complete Guide (incl Hooks, React Ro
 - Components dispatch **actions**
 - Components dispatch actions -> actions forwarded to reducer -> reducer turns out new state -> subscribing components are notified so they can update their UI
 - Goal is to do different things inside of reducer for different actions
+- Redux **does not merge new state with the existing state**, instead it takes what is returned and replaces the existing state with it
+- Should *never* mutate existing state, always overwrite it with new state
 
 ### Reducer Function
 
@@ -26,3 +28,55 @@ This is the redux portion from [React - The Complete Guide (incl Hooks, React Ro
 - Output : New State Object
 - Should be a pure function - same input should always produce the same output
 - Returned new state replaces the existing state
+
+## Redux imports
+
+### useSelector()
+
+- Gives access to data managed in the store
+- Gets a function which receives state managed by redux
+- Returns part of state to extract
+- React-Redux *automatically* sets up subscription to redux-store for this component
+- If component is ever unmounted, react-redux automatically clears subscription
+
+### useDispatch()
+
+- Gives back a dispatch function which can be executed
+- Action is an object with a type property
+
+### connect()
+
+- Helps with class based components
+- Higher-order component
+- Execute connect function, which returns a new function which is passed the component 
+- Takes 2 function arguments
+  - 1. Maps redux state to props
+    - returns object where keys are available as props in the receiving component
+    - values are logic for drilling into redux state
+  - 2. Maps dispatch to props
+
+## Redux Toolkit
+
+### createSlice()
+
+- Takes an object with 3 keys
+  ```js
+  createSlice({
+    name: 'name of slice',
+    initialState: {},
+    reducers: {
+      // actionType(state) { state.toUpdate }
+      // e.g.
+      increment(state) {
+        state.counter++
+      }
+    }
+  })
+  ```
+  - Uses some other library and makes sure that state CANNOT be mutated, instead returns a copy with only changes in the reducer made
+  - Can make 'slices' of reducers to separate concerns 
+- Still only have 1 store
+
+### configureStore()
+
+- Takes an object as an argument with key `reducer` that itself takes another object with keys for each separate reducer that is going into the store
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,87 @@
-import classes from './Counter.module.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { counterActions } from '../store/counter-slice'
+import classes from './Counter.module.css'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => state.counter.counter)
+  const show = useSelector((state) => state.counter.showCounter)
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment())
+  }
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5))
+  }
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement())
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter
+
+// import { Component } from 'react'
+// import { connect } from 'react-redux'
+
+// class Counter extends Component {
+//   // no constructor because no state
+//   incrementHandler() {
+//     this.props.increment()
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement()
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     )
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     decrement: () => dispatch({ type: 'decrement' })
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter)
